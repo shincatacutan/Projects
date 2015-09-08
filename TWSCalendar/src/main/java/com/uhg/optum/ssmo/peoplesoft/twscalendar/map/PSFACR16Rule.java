41e82e938d192c632935271385ad3c55087b474f
scatacut <sheena_catacutan@optum.com>
2015-09-09 01:02:15 +0800

    fixed PSFACR16Rule

--- TWSCalendar/src/main/java/com/uhg/optum/ssmo/peoplesoft/twscalendar/map/PSFACR16Rule.java
+++ TWSCalendar/src/main/java/com/uhg/optum/ssmo/peoplesoft/twscalendar/map/PSFACR16Rule.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import org.joda.time.DateTimeConstants;
 import org.joda.time.LocalDate;
 
 import com.uhg.optum.ssmo.peoplesoft.twscalendar.domain.CalendarDay;
@@ -24,56 +23,43 @@ public class PSFACR16Rule extends CalendarJobRule {
 	@Override
 	public List<CalendarDay> getFinalDates() {
 		List<CalendarDay> result = new ArrayList<CalendarDay>();
-		
+
 		for (LocalDate d : getResults()) {
 			result.add(new CalendarDay(Boolean.FALSE, d));
 		}
 
-		
 		CalendarUtils.addHolidaysToList(result, holidays);
-		Collections.sort(result,new CalendarDayComparator());
+		Collections.sort(result, new CalendarDayComparator());
 		return result;
 	}
 
 	/*
-	 * PSF_ACR_CL_WEEKDAYS PSFACR16 Runs on weekday except {{PSFACR11}} holiday,
-	 * wd1 , lwd, friday and 2 days working day before 10
+	 * Runs every weekdays except holidays, workday 1, last work day and the
+	 * days that PSFACR11 runs
 	 */
-	public List<LocalDate> getWeekdayExceptWD1LWDFriday(LocalDate calendar) {
+	
+	@Override
+	public List<LocalDate> getResults() {
 		List<LocalDate> listDays = new ArrayList<LocalDate>();
-		while (calendar.getDayOfMonth() <= calendar.dayOfMonth()
-				.getMaximumValue()) {
-			if (!CalendarUtils.isWeekEnds(calendar)
-					&& calendar.getDayOfWeek() != DateTimeConstants.FRIDAY
-					&& !CalendarUtils.isHoliday(calendar, holidays)) {
-				listDays.add(new LocalDate(calendar.getYear(), calendar
-						.getMonthOfYear(), calendar.getDayOfMonth()));
-			}
-			if (calendar.getDayOfMonth() == calendar.dayOfMonth()
-					.getMaximumValue()) {
-				break;
-			}
-			calendar = calendar.plusDays(1);
+		for (int i = 1; i <= 12; i++) {
+			List<LocalDate> dates = getWeekdayExceptWD1LWDFriday(new LocalDate(
+					year, i, 1));
+			listDays.addAll(dates);
 		}
-		
-		PSFACR11Rule rule = new PSFACR11Rule(year, holidays);
-		listDays.removeAll(rule.getResults());
-		listDays.remove(CalendarUtils.getWorkDay1(calendar, holidays));
+		return listDays;
+	}
+	
+	private List<LocalDate> getWeekdayExceptWD1LWDFriday(LocalDate calendar) {
+		List<LocalDate> listDays = new ArrayList<LocalDate>();
+		listDays = CalendarUtils.getAllWorkDays(calendar.getMonthOfYear(),
+				year, holidays);
+		listDays.removeAll(new PSFACR11Rule(year, holidays).getResults());
+		listDays.remove(CalendarUtils.getNthWorkDayOfMonth(1,
+				calendar.getMonthOfYear(), year, holidays));
+
 		CalendarUtils.removeHolidays(listDays, holidays);
 		return listDays;
 	}
 
-	@Override
-	public List<LocalDate> getResults() {
-		 List<LocalDate> listDays = new ArrayList<LocalDate>();
-		for (int i = 1; i <= 12; i++) {
-
-			List<LocalDate> dates = getWeekdayExceptWD1LWDFriday(new LocalDate(
-					year, i, 1));
-			for (LocalDate d : dates) {
-				listDays.add(d);
-			}
-		}
-		return listDays;
-	}
+	
 }
