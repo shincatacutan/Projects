389e4d3de1640d2b9008c7a147593e8b642eb49e
MasterDee <MasterDee@MasterDee-PC>
2015-09-10 17:42:31 +0800

    Added JunitMock Class for JUnit Added PSFACR09 Rule, Test Classes

---
+++ TWSCalendar/src/test/java/com/uhg/optum/ssmo/peoplesoft/twscalendar/map/PSFACR09RuleTest.java
@@ -0,0 +1,44 @@
+package com.uhg.optum.ssmo.peoplesoft.twscalendar.map;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.uhg.optum.ssmo.peoplesoft.twscalendar.domain.CalendarDay;
+
+public class PSFACR09RuleTest extends JunitMock{
+
+	@Before
+	public void setUp() throws Exception {
+		listExpectedDates();
+		actualDates = new ArrayList<LocalDate>();
+		PSFACR09Rule psfacro1rule = new PSFACR09Rule(2015,listHolidays());
+		for(CalendarDay cal : psfacro1rule.getFinalDates()){
+			if(!cal.isHoliday())
+				actualDates.add(cal.getCalDay());
+		}
+	}
+
+	@Test
+	public void testGetFinalDates() {
+		System.out.println(actualDates.get(3).toString());
+		assertTrue("Actual Dates must have January 24,26 2015",actualDates.containsAll(expectedDates));
+		assertTrue("Actual Dates total must be 12",actualDates.size()==listSize);
+	}
+
+	@Override
+	public void listExpectedDates() {
+		expectedDates = new ArrayList<LocalDate>();
+		expectedDates.add(new LocalDate(2015,1,23));
+		expectedDates.add(new LocalDate(2015,1,27));
+		expectedDates.add(new LocalDate(2015,9,23));
+		expectedDates.add(new LocalDate(2015,9,25));
+		listSize = 24;
+		
+	}
+
+}
